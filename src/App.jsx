cf28f0508b9b45132f99a42bddd234d81bc81abf
sahillohan33 <sahillohan33@gmail.com>
2024-01-07 13:53:30 +0530

    Added React-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import './App.css';
 
+import {Routes} from 'react-router-dom';
 function App() {
   
 
   return (
     <>
-    hello
+    <Routes>
+      
+
+    </Routes>
     </>
      )
 }
